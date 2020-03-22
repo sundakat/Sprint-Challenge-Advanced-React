5064148fa4d775d05a7d81da10276a8f078861ce
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2020-03-21 19:12:44 -0500

    Added darkmode hook

---
+++ client/src/hooks/useDarkMode.js
@@ -0,0 +1,17 @@
+import {useEffect} from 'react';
+import {useLocalStorage} from './useLocalStorage';
+
+export const useDarkMode = () => {
+  const [darkMode, setDarkMode] = useLocalStorage('dark_mode',false);
+
+  useEffect(()=> {
+    const body = document.querySelector('body');
+    if (darkMode) {
+      body.classList.add('dark-mode');
+    } else {
+      body.classList.remove('dark-mode');
+    }
+  },[darkMode])
+
+  return [darkMode, setDarkMode]
+}
